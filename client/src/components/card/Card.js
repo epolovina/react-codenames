1d42707ceff378fbb0086559734c024a4c9c6757
TimTran8 <tim_tran@live.ca>
2020-06-08 23:00:20 -0700

    made tiles have color assigned

--- client/src/components/card/Card.js
+++ client/src/components/card/Card.js
@@ -7,56 +7,23 @@ class Card extends Component {
     super();
 
     this.state = {
-      black: true,
-      blue: true,
-      red: true,
-      color: 0
+      colour: true,
+      clicked: false
     }
   }
 
-  showCard() {
-    console.log(this.props);
-    console.log(this.state);
-    
-    if (this.props.color == "dark") {
-      this.setState({
-        color: 1
-      });
-    }
-    else if(this.props.color == "blue") {
-      this.setState({
-        color: 2
-      });
-    }
-    else if (this.props.color == "red") {
-      this.setState({
-        color: 3
-      });
+  changeColor() {
+    if (!this.clicked) {
+      this.setState({colour: !this.state.colour});
+      this.clicked = true;
     }
   }
 
   render() {
     let setColor;
-    if(this.state.color == 1) {
-      setColor = "dark";
-    }
-    else if (this.state.color == 2) {
-      setColor = "primary"
-    }
-    else if (this.state.color == 3) {
-      setColor = "danger"
-    }
-    else {
-      setColor = "outline-primary"
-    }
-    
-    // let setBlack = this.state.black ? "outline-primary" : "dark"
-    // let setBlue = this.state.black ? "outline-primary" : "primary"
-    // let setRed = this.state.black ? "outline-primary" : "danger"
-
-    const props = this.props;
+    setColor = this.state.colour ? "outline-primary" : this.props.color;
     return (
-      <Button variant={setColor} className="card-body buttonClicked" onClick={this.showCard.bind(this)}>
+      <Button variant={setColor} className="card-body buttonClicked" onClick={this.changeColor.bind(this)} >
         {this.props.name}
       </Button>
     );
